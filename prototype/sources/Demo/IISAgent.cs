570c4d90164d8e4ea29893984c6b768e97a0e0eb
aleksandr.karifanov <aleksandr.karifanov@ESW481>
2016-12-15 12:14:40 +0300

    iis starting was fixed

--- prototype/sources/Demo/IISAgent.cs
+++ prototype/sources/Demo/IISAgent.cs
@@ -3,7 +3,7 @@ using System.Diagnostics;
 
 namespace Demo
 {
-    class IISAgent:IDisposable
+    class IISAgent : IDisposable
     {
         Process process;
 
--- prototype/sources/Demo/Program.cs
+++ prototype/sources/Demo/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.IO;
 using AutoMapper;
 using System.Diagnostics;
+using System.Runtime.InteropServices;
 using Newtonsoft.Json;
 
 using SocialHuman;
@@ -16,44 +17,90 @@ namespace Demo
     using Models.Input;
     using Models.Output;
     using Parsers;
+    
 
     class Program
     {
         static string inputFilePath = Path.Combine(Environment.CurrentDirectory, "input.json");
         static string outputDirectory = Path.Combine(Environment.CurrentDirectory, "OutputTemplate");
         static string outputFilePath = Path.Combine(outputDirectory, "output.json");
-        static string iisPort = "8877";
 
         static Algorithm algorithm;
 
+        static IISAgent iis;
+
+
+        #region Trap application termination
+        [DllImport("Kernel32")]
+        private static extern bool SetConsoleCtrlHandler(EventHandler handler, bool add);
+
+        private delegate bool EventHandler(CtrlType sig);
+        static EventHandler _handler;
+
+        enum CtrlType
+        {
+            CTRL_C_EVENT = 0,
+            CTRL_BREAK_EVENT = 1,
+            CTRL_CLOSE_EVENT = 2,
+            CTRL_LOGOFF_EVENT = 5,
+            CTRL_SHUTDOWN_EVENT = 6
+        }
+
+        private static bool Handler(CtrlType sig)
+        {
+            //iis killing
+            if(iis != null)
+            {
+                iis.Dispose();
+            }
+
+            return true;
+        }
+        #endregion
+
         static void Main(string[] args)
         {
+            _handler += new EventHandler(Handler);
+            SetConsoleCtrlHandler(_handler, true);
+
+            Program program = new Program();
+
+            program.Start();
+        }
+
+        
+        void Start()
+        {
             ConfigureMapper();
+
             try
             {
                 Initialize();
 
                 Run();
 
-                using (IISAgent iis = new IISAgent())
+                using (iis = new IISAgent())
                 {
-                    iis.Start($"/path:\"{outputDirectory}\" /port:{iisPort} /clr:v2.0");
+                    Random rand = new Random();
+                    int port = rand.Next(30000, 40000);
 
-                    Process.Start($"http://localhost:{iisPort}");
+
+                    iis.Start($"/path:\"{outputDirectory}\" /port:{port} /clr:v2.0");
+
+                    Process.Start($"http://localhost:{port}");
 
                     WaitKeyPress();
                 }
             }
             catch (JsonException ex)
             {
-                Console.WriteLine("\n\nERROR! Incorrect input: " + ex.Message);
+                Console.WriteLine("\n\nERROR! Input is incorrect : " + ex.Message);
                 WaitKeyPress();
             }
         }
 
-        static void ConfigureMapper()
+        void ConfigureMapper()
         {
-            //todo: clear
             Console.WriteLine("Configurating....");
 
 
@@ -71,7 +118,7 @@ namespace Demo
             });
         }
 
-        static void Initialize()
+        void Initialize()
         {
             //todo: clear
             Console.WriteLine("Initializing....");
@@ -88,7 +135,7 @@ namespace Demo
                 Mapper.Map<Dictionary<string, PeriodInitialStateParameters>>(periodState));
         }
 
-        static void Run()
+        void Run()
         {
             Console.WriteLine("Algorithm is running....");
 
@@ -148,7 +195,7 @@ namespace Demo
             File.WriteAllText(outputFilePath, outputString);
         }
 
-        private static void WaitKeyPress()
+        void WaitKeyPress()
         {
             Console.WriteLine("{0}Press any key to continue...", Environment.NewLine);
             Console.ReadKey();
